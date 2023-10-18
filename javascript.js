86ade7b8ec8ca70789183c57f2dfce79106892f1
Gabriel Vega <glvega.cc@gmail.com>
2023-10-19 00:21:01 +0800

    Make 16x16  grid using only javascript

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const container = document.getElementById('container')
+
+function makeSquares(rows, columns) {
+    container.style.setProperty('--gridRows', rows)
+    container.style.setProperty('--gridColumns', columns)
+    for (let i = 0; i < (rows * columns); i++) {
+        let square = document.createElement('div');
+        square.innerText = (i + 1);
+        container.appendChild(square).className = "gridItem"
+    }
+}
+
+makeSquares(16,16)
