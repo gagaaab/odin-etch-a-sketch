739276c2910a2ee543db173820f2f3792cee071b
Gabriel Vega <glvega.cc@gmail.com>
2023-10-20 01:26:35 +0800

    Change hover effect to click and drag

--- javascript.js
+++ javascript.js
@@ -5,7 +5,6 @@ function makeSquares(sides) {
     container.style.setProperty('--gridColumns', sides)
     for (let i = 0; i < (sides * sides); i++) {
         let square = document.createElement('div');
-        square.innerText = (i + 1);
         container.appendChild(square).className = "gridItem"
     }
 }
@@ -30,7 +29,30 @@ makeSquares (squares)
 
 const gridItem = document.querySelector('.gridItem')
 
-container.addEventListener('mouseover', () => {
+container.addEventListener('click', () => {
     if (event.target.matches('.gridItem')) {
-        event.target.classList.toggle('hovered');
-}});
+        event.target.classList.add('hovered');
+    }
+});
+
+let mouseDown = false;
+
+container.addEventListener('mousedown', () => {
+    mouseDown = true
+    document.addEventListener("mousemove", onMouseMove)
+    document.addEventListener("mouseup", onMouseUp)
+});
+
+function onMouseMove(event) {
+    if (mouseDown) {
+        if (event.target.matches('.gridItem')) {
+            event.target.classList.add('hovered');
+        }
+    }
+}
+
+function onMouseUp(event) {
+    mouseDown = false;
+    document.removeEventListener("mousemove", onMouseMove)
+    document.removeEventListener("mouseup", onMouseUp)
+}
